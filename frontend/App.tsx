3162aa315512a87eb53729bb77e200b4d2a2b490
Ali AlWahayb <alialwahayb@outlook.com>
2025-02-21 09:58:55 +0300

    test1

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,13 @@
-import HomeView from 'components/Home/HomeView';
-import { View } from 'react-native';
+import { ScreenContent } from 'components/ScreenContent';
+import { StatusBar } from 'expo-status-bar';
+
+import './global.css';
 
 export default function App() {
   return (
     <>
-      <View className="w-100 h-100 bg-black">
-        <HomeView />
-      </View>
+      <ScreenContent title="Home" path="App.tsx" />
+      <StatusBar style="auto" />
     </>
   );
 }
