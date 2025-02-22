c9f583392c66a23ddec4228d9c35b58cfacb9c02
Ahas166 <a_bodi_sa@yahoo.com>
2025-02-22 17:40:26 +0300

    Update Buttons
    Update Buttons

--- frontend/components/Shared/components/FormElements/Buttons.tsx
+++ frontend/components/Shared/components/FormElements/Buttons.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Text,View, TouchableOpacity } from 'react-native';
 
 
-const Buttons = () => {
+const Buttons = ({title , icon}) => {
     return (
-        <View className="flex items-center justify-center mt-6 w-full px-8">
+      
         <TouchableOpacity className="bg-green-400 rounded-full py-2 px-20 ">
-          <Text className="text-white text-base">Button</Text>
+          <Text className="text-white text-base">{title}</Text>
         </TouchableOpacity>
-      </View>
+    
     )
 };
 
