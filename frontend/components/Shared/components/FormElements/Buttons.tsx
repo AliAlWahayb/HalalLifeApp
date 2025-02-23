e6f8067d5a36a3650a04471dc067554ff60e79b9
Ahas166 <a_bodi_sa@yahoo.com>
2025-02-23 22:31:40 +0300

    Update Buttons and creating InputForm

--- frontend/components/Shared/components/FormElements/Buttons.tsx
+++ frontend/components/Shared/components/FormElements/Buttons.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Text,View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react'; 
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const Buttons = ({title,onPress}) => {
+const [isLoading, setIsLoading] = useState(false);
+
+  const handlePress = () => {
+    onPress();
 
 
-const Buttons = ({title , icon}) => {
-    return (
-      
-        <TouchableOpacity className="bg-green-400 rounded-full py-2 px-20 ">
+  };
+
+  return (
+    <View className="flex items-center justify-center mt-6 w-full px-8">
+      <TouchableOpacity
+        className="bg-green-500 rounded-full py-2 px-20"
+        onPress={handlePress} 
+      >
           <Text className="text-white text-base">{title}</Text>
-        </TouchableOpacity>
-    
-    )
+        
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Buttons;
